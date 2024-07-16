782feb97b98a2c2f202d2252c05c53246575f24e
abdallah4356 <abdallahelmohammady@gmail.com>
2024-07-16 00:45:00 -0700

    gh

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(0, 9):
     for y in range(1, 10):
         if i != y:
             if i != 8 or y != 9:
-                print(f"{i}{y}", end=', ')
+                print("{}{}".format(i, y), end=', ')
             else:
-                print(f"{i}{y}")
+                print("{}{}".format(i, y))
