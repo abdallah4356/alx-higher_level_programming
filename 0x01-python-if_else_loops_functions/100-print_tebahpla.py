a86e878c28506cc21ebcfda34ce74ad4f9ec2495
abdallah4356 <abdallahelmohammady@gmail.com>
2024-07-22 06:06:48 -0700

    G

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(25, -1, -1):
+for i in range(0, 26):
     if i % 2 == 0:
         print("{}".format(chr(122 - i)), end="")
 
     else:
-        print("{}".format(chr(90 - i)), end="")
+        print("{}".format(chr(122 - i - 32)), end="")
