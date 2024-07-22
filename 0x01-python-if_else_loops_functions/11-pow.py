de1a0e41ef7a323a85ec724525ec249b746a4338
abdallah4356 <abdallahelmohammady@gmail.com>
2024-07-22 08:35:51 -0700

    G

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    r = 1
-    for i in range(b):
-        r *= a
-    return r
+    if b > 0:
+        r = 1
+        for i in range(b):
+            r *= a
+        return r
+    elif b < 0:
+        r = 1
+        for i in range (-b):
+            r *= a
+        return 1 / r
