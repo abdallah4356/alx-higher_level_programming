66491e533e873f005ab40f8d2b0d40e614f5f839
abdallah4356 <abdallahelmohammady@gmail.com>
2024-07-25 06:41:11 -0700

    G

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div as ad, s, m, d
+    from calculator_1 import add as ad, sub as s, mul as m, div as d
 
     a = 10
     b = 5
