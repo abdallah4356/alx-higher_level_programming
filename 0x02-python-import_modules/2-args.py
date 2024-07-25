007898819a21f81ed8cf3454ed8ad8c0495e0c21
abdallah4356 <abdallahelmohammady@gmail.com>
2024-07-25 12:46:11 -0700

    GH

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 1:
         print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:\n1: {}".format(len(sys.argv) - 1, sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
 
