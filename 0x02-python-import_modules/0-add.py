ad408b70c8b8a48d96c35b905d5496ebf893a64f
abdallah4356 <abdallahelmohammady@gmail.com>
2024-07-25 06:28:49 -0700

    G

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add as s
 
-from add_0 import add as s
+    a = 1
+    b = 2
 
-a = 1
-b = 2
-
-result = s(a, b)
-
-print("{} + {} = {}".format(a, b, result))
+    result = s(a, b)
+    print("{} + {} = {}".format(a, b, result))
