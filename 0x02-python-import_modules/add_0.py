84d6bef1f7d41c8a7d0a7cc9cc3afdf77d0e45ed
abdallah4356 <abdallahelmohammady@gmail.com>
2024-07-25 06:20:16 -0700

    G

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,12 +1,7 @@
 def add(a, b):
-    if __name__ == "__main__":
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
     return (a + b)
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
