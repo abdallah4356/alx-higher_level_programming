12526985163e77b40b401d723d5d0681d544a55b
abdallah4356 <abdallahelmohammady@gmail.com>
2024-08-10 12:20:37 -0700

    GH

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 
 def element_at(my_list, idx):
     if idx < 0:
-        return()
+        return(None)
     elif idx > (len(my_list)):
-        return()
+        return(None)
     else:
         my_list.pop(idx)
