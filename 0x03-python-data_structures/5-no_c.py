a5b5132d12e5889660e25a247b321c682691a85a
abdallah4356 <abdallahelmohammady@gmail.com>
2024-08-11 20:33:36 -0700

    GH

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,8 @@
 
 def no_c(my_string):
     while c and C in my_string:
-        new = my_string.replace(c, C)
+        for i in range(len(my_string)):
+            if my_string[i] == "c" or my_string[i] == "C":
+                continue
+            new = my_string[i]
     return new
